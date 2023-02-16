6eb4426a89ff6a9d2cbaaee587bd43b09d205e0f
chatelo <biikate48@gmail.com>
2023-02-16 08:59:47 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/*use betty style
- * this code is nice
- */
+/*use betty style */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
