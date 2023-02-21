77da903117a879f041a8dad9ebb7389ef9bc1955
chatelo <biikate48@gmail.com>
2023-02-21 13:20:27 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+/* Declare char */
+char c;
+/* for loop condition */
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+/* prints new line */
+_putchar('\n');
+}
