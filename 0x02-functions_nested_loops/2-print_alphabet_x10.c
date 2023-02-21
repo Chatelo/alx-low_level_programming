a22e8ceb16a25a6db386571c1ba760ee09644cd8
chatelo <biikate48@gmail.com>
2023-02-21 13:29:50 -0800

    10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+* @void: When parameter X is void, it means it is not acceptions any arg
+* Description: Write a function that prints 10 times the alphabet
+* in lowercase, followed by a new line.
+** main.h header: contains prototypes of all your functions
+* and the prototype of the function _putchar)*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+/*Declaration*/
+int i;
+
+/*for loop*/
+for (i = 0; i < 10; i++)
+{
+/*Declaration*/
+char c;
+
+/*for loop*/
+for (c = 'a'; c <= 'z'; c++)
+{
+/*prints alphabetical letters*/
+_putchar(c);
+}
+/*prints new line*/
+_putchar('\n');
+}
+}
