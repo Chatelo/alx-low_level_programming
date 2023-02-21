e39bc391bd9fe78084037b7448e911414e5f43e4
chatelo <biikate48@gmail.com>
2023-02-21 12:54:26 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* main - Entry point
+* @void: When parameter X is void, it means it is not acceping  any arg
+* Description: program that prints _putchar, followed by a new line
+* main.hr: performing input and output)*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
