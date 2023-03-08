cbf5b32b7e257f2037ff6acb8ef5da839b13fb4d
chatelo <biikate48@gmail.com>
2023-03-08 09:06:23 -0800

    replaced 0-puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
+
 /**
- * _puts_recursion - puts a string to stdout recursively
- *
- * @s: string to put to stdout
- *
- * Return: always void
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
-	if (!*s) /* if s points to null char */
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+  if (*s == '\0')
+  {
+    _putchar('\n');
+    return;
+  }
+  _putchar(*s);
+  _puts_recursion(s + 1);
 }
