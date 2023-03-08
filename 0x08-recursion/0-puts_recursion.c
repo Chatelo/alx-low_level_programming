2fb71d5f1640ba3b5d18bfd6521d09def43bb353
chatelo <biikate48@gmail.com>
2023-03-08 08:57:29 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,8 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
+	_putchar('\n');
