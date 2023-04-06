53bbdcdfb66474499dc9a5dc840bad4724ebc0e5
chatelo <biikate48@gmail.com>
2023-04-06 04:27:19 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,23 @@
  * there is one or more chars in the string b that is not 0 or 1
  * b is NULL
  */
-unsigned int binary_to_uint(const char *b)
-{
-unsigned int n = 0;
+unsigned int binary_to_uint(const char *b) {
+    unsigned int result = 0;
 
-if (b == NULL)
-return (0);
+    if (b == NULL) {
+        return 0;
+    }
 
-while (*b)
-{
-if (*b != '0' && *b != '1')
-return (0);
+    for (int i = 0; b[i] != '\0'; i++) {
+        if (b[i] == '0') {
+            result = (result << 1) | 0;
+        } else if (b[i] == '1') {
+            result = (result << 1) | 1;
+        } else {
+            return 0;  // invalid character
+        }
+    }
 
-n = (n << 1) + (*b - '0');
-b++;
+    return result;
 }
-return (n);
-}
+
