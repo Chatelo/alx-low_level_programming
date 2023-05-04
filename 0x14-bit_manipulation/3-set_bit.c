89286599e9c907dd34051beaa5d722cd716a6906
chatelo <biikate48@gmail.com>
2023-05-04 22:28:15 +0300

    bit-manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index...
+ * @n: pointer to an unsigned long integer
+ * @index: index of the bit to set, starting from zero
+ *
+ * Return: 1 if it worked, or -1 if an error occurred in the program
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1);
+
+*n = *n | (1 << index);
+return (1);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.....
+ *
+ * Return: zero if big endian, one if little endian
+ */
+int get_endianness(void)
+{
+unsigned int x = 1;
+char *c = (char *) &x;
+return ((int)(*c));
+}
