0fb10ad254b0a4034050ef34ce22d7f2efea019c
Benard Ronoh <biikate48@gmail.com>
2023-06-15 14:32:43 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - inserts new node at given position
- * @h: pointer to the head of list
- * @idx: index of the list where new node should be added (starting from 0)
- * @n: data to be stored in new node
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the head of the list
+ * @idx: index of the list where the new node should be added (starting from 0)
+ * @n: data to be stored in the new node
  *
- * Return: address of the new node, or NULL if it fail
+ * Return: address of the new node, or NULL if it failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **b, unsigned int idx, int a)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node, *current;
-unsigned int a;
+unsigned int i;
 
-if (b == NULL)
+if (h == NULL)
 return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
-new_node->a = a;
+new_node->n = n;
 
 if (idx == 0)
 {
 new_node->prev = NULL;
-new_node->next = *b;
-if (*b != NULL)
-(*b)->prev = new_node;
-*b = new_node;
+new_node->next = *h;
+if (*h != NULL)
+(*h)->prev = new_node;
+*h = new_node;
 return (new_node);
 }
 
-current = *b;
+current = *h;
 for (i = 0; i < idx - 1 && current != NULL; i++)
 current = current->next;
 
