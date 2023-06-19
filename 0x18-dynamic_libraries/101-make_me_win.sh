bdc79fc547c0bd2727c0bbceac1993fb9395d36c
Benard Ronoh <biikate48@gmail.com>
2023-06-19 11:11:16 -0700

    Make Julien win Jakcpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+ls -la
+history -c
+clear
+ls -la
+md5_output=$(md5sum gm)
+expected_md5="d52e6c18e0723f5b025a75dea19ef365  gm"
 
-echo "int rand(void) { return 6; }" > rand.c
-gcc -shared -o fake_rand.so -fPIC rand.c
-rm rand.c
-export LD_PRELOAD=./fake_rand.so
+if [ "$md5_output" = "$expected_md5" ]; then
+    echo "MD5 verification successful. Proceeding with winning the jackpot."
+    echo -e "#include <stdio.h>\n#include <stdlib.h>\n\nvoid win_the_jackpot(void) __attribute__((constructor));\n\nvoid win_the_jackpot(void) {\n    system(\"echo 'Please make me win!' >> win.txt\");\n}" > win.c
+    gcc -shared -o win.so -fPIC win.c
+    rm win.c
+    export LD_PRELOAD=./win.so
+else
+    echo "MD5 verification failed. Exiting without winning the jackpot."
+fi
 
